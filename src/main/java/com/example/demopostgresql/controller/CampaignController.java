0f1cd190870e193f8495c218fbb940123e4ace0e
rishiraj <rishi.raj@gdn-commerce.com>
2020-10-17 18:48:06 +0530

    commit

--- src/main/java/com/example/demopostgresql/controller/CampaignController.java
+++ src/main/java/com/example/demopostgresql/controller/CampaignController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;

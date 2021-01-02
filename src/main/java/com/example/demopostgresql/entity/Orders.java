319c932b7cff69254cf0e853aa2ab6eefa425834
rishiraj <rishi.raj@gdn-commerce.com>
2021-01-02 21:11:30 +0530

    commit

--- src/main/java/com/example/demopostgresql/entity/Orders.java
+++ src/main/java/com/example/demopostgresql/entity/Orders.java
@@ -22,7 +22,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;

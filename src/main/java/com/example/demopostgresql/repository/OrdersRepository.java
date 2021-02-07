417b9d3f9e3327a8a02c88a80e22b6ce86fdf212
rishiraj <rishi.raj@gdn-commerce.com>
2021-02-07 17:02:11 +0530

    commit

--- src/main/java/com/example/demopostgresql/repository/OrdersRepository.java
+++ src/main/java/com/example/demopostgresql/repository/OrdersRepository.java
@@ -3,6 +3,7 @@ package com.example.demopostgresql.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,4 +18,15 @@ public interface OrdersRepository extends JpaRepository<Orders, String> {
 
   @Query("SELECT o from Orders o where o.id = :id")
   List<Object> findOut(@Param("id") String id);
+
+  @Modifying(clearAutomatically = true)
+  @Query("Delete from Orders where id = ?1")
+  void del(String orderId);// This query will not delete the child entity
+
+  /**
+   * Open questions :
+   *
+   How to modify/delete specific child entity if we have parent id? -> Got the answer
+   What is the ideal way to insert child entity when inserting parent entity? --> Need to go through code
+   */
 }
